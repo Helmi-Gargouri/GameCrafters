ff71829bc0723e0975946231d778ae7b684d107f
Ihebmsaed <ihebmsaed41@gmail.com>
2025-03-07 00:52:58 +0100

    final

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./*.html", "./public/**/*.html", "./src/**/*.js"],
+  content: [
+    "./public/index.html", 
+    "./public/profile-daily.html",
+    "./public/exercice1.html",
+    "./public/exercice2.html",
+  ],
   theme: {
     extend: {
       colors: {
-        primary: "#7f00ff",
-        secondary: "#1a0b25",
-        background: "#2d133d",
+        primary: "#301529", 
+        secondary: "#4E2143", 
+        accent: "#9C3FE4", // Couleur degradd
+        accent2: "#F8049C", //2
+        gray: {
+          400: "#9CA3AF", // les icônes
+          600: "#4B5563", 
+          800: "#1F2937", 
+        },
+      },
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"], // Police Poppins
       },
     },
   },
